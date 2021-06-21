acbbc777ab4ff27574935a4f7d1a2a255a880b25
Pashamks <74491257+Pashamks@users.noreply.github.com>
2021-06-21 08:36:19 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Ultimate_Predictor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.bPredict = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.progressBar1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.bPredict);
+            this.splitContainer1.Size = new System.Drawing.Size(478, 287);
+            this.splitContainer1.SplitterDistance = 133;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar1.Location = new System.Drawing.Point(0, 0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(474, 129);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // bPredict
+            // 
+            this.bPredict.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bPredict.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bPredict.Location = new System.Drawing.Point(0, 0);
+            this.bPredict.Name = "bPredict";
+            this.bPredict.Size = new System.Drawing.Size(474, 146);
+            this.bPredict.TabIndex = 0;
+            this.bPredict.Text = "PREDICT";
+            this.bPredict.UseVisualStyleBackColor = true;
+            this.bPredict.Click += new System.EventHandler(this.bPredict_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 287);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button bPredict;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Ultimate_Predictor
+{
+   
+    public partial class Form1 : Form
+    {
+        private const string APP_NAME = "ULTIMATE_PREDICTIONS";
+        private readonly string PATH = $"{Environment.CurrentDirectory}//PredictionConfig.json";
+        private string[] _predictions;
+        private Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void bPredict_Click(object sender, EventArgs e)
+        {
+            bPredict.Enabled = false;
+            await Task.Run(() =>
+            {
+                for (int i = 1; i <= 100; ++i)
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        UpdateProgressBar(i);
+                        Text = $"{i}%";
+                    }));
+
+                    Thread.Sleep(20);
+                }
+
+            });
+            int index = rand.Next(_predictions.Length);
+            MessageBox.Show(_predictions[index]);
+            bPredict.Enabled = true;    
+            progressBar1.Value = 0;
+            Text = APP_NAME;
+        }
+        private void UpdateProgressBar(int i)
+        {
+            if(i == progressBar1.Maximum)
+            {
+                progressBar1.Maximum = i + 1;
+                progressBar1.Value = i + 1;
+                progressBar1.Maximum = i;
+            }
+            else
+            {
+                progressBar1.Value = i + 1;
+            }
+            progressBar1.Value = i;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text = APP_NAME;
+            try
+            {
+                string data = File.ReadAllText(PATH);
+                _predictions = JsonConvert.DeserializeObject <string[]> (data);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if(_predictions == null)
+                {
+                    Close();
+                }
+                else if(_predictions.Length == 0)
+                {
+                    MessageBox.Show("Predictions are over!");
+                    Close();
+                }
+            }
+        }
+    }
+}
