c63524fe068435bdef1c93b0094a74a2892ae21c
Pashamks <74491257+Pashamks@users.noreply.github.com>
2021-06-21 10:42:19 +0300

    add line edit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,7 +31,9 @@ namespace Ultimate_Predictor
         {
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
             this.bPredict = new System.Windows.Forms.Button();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -53,9 +55,11 @@ namespace Ultimate_Predictor
             // 
             // splitContainer1.Panel2
             // 
+            this.splitContainer1.Panel2.Controls.Add(this.maskedTextBox1);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
             this.splitContainer1.Panel2.Controls.Add(this.bPredict);
-            this.splitContainer1.Size = new System.Drawing.Size(478, 287);
-            this.splitContainer1.SplitterDistance = 133;
+            this.splitContainer1.Size = new System.Drawing.Size(614, 368);
+            this.splitContainer1.SplitterDistance = 170;
             this.splitContainer1.TabIndex = 0;
             // 
             // progressBar1
@@ -63,26 +67,42 @@ namespace Ultimate_Predictor
             this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.progressBar1.Location = new System.Drawing.Point(0, 0);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(474, 129);
+            this.progressBar1.Size = new System.Drawing.Size(610, 166);
             this.progressBar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(161, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter your question:";
             // 
             // bPredict
             // 
-            this.bPredict.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bPredict.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.bPredict.Location = new System.Drawing.Point(0, 0);
+            this.bPredict.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bPredict.Location = new System.Drawing.Point(0, 82);
             this.bPredict.Name = "bPredict";
-            this.bPredict.Size = new System.Drawing.Size(474, 146);
+            this.bPredict.Size = new System.Drawing.Size(610, 108);
             this.bPredict.TabIndex = 0;
             this.bPredict.Text = "PREDICT";
             this.bPredict.UseVisualStyleBackColor = true;
             this.bPredict.Click += new System.EventHandler(this.bPredict_Click);
             // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(-2, 54);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(614, 22);
+            this.maskedTextBox1.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(478, 287);
+            this.ClientSize = new System.Drawing.Size(614, 368);
             this.Controls.Add(this.splitContainer1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
@@ -93,6 +113,7 @@ namespace Ultimate_Predictor
             this.Load += new System.EventHandler(this.Form1_Load);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -104,6 +125,8 @@ namespace Ultimate_Predictor
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Button bPredict;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
     }
 }
 
